97b14af28b017c422fa259413bc30380f59902b1
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-04-05 18:17:52 +0200

    add IDIV_capture_ecran.lua
    add IDIV_capture_ecran.lua

---
+++ examples/IDIV_capture_ecran.lua
@@ -0,0 +1,415 @@
+--This script is a simple documentation of a tree in a Lua table
+
+
+--1. basic data
+
+--1.1 libraries
+require("iuplua")           --require iuplua for GUIs
+
+--1.1.1 libraries for images
+require("cdlua") --for images
+require("cdluapdf") --for pdf and powerpoint
+require("imlua") --for images
+require("cdluaim") --for screen capture
+require("iupluacd") --for iup canvas
+
+--1.1.2 libraries for videos
+--require("imlua_wmv") --format .WMV for saving video
+require("imlua_avi") --format .AVI for saving video
+
+
+--1.3 initalize clipboard
+clipboard=iup.clipboard{}
+
+--1.2 color section
+--1.2.1 color of the console associated with the graphical user interface if started with lua54.exe and not wlua54.exe
+os.execute('color 71')
+
+--1.2.2 colors
+color_red="135 131 28"
+color_light_color_grey="96 197 199"
+color_grey="162 163 165"
+color_blue="18 132 86"
+
+--1.2.3 color definitions
+color_background=color_light_color_grey
+color_buttons=color_blue -- works only for flat buttons
+color_button_text="255 255 255"
+color_background_tree="246 246 246"
+
+
+--1.3 path of the graphical user interface and filename of this script
+path=arg[0]:match("(.*)\\")
+--test with: print(path)
+thisfilename=arg[0]:match("\\([^\\]+)$")
+--test with: print(arg[0])
+--test with: print(thisfilename)
+
+path_documentation_tree=path .. "\\" .. "screenshot_tree.lua"
+
+
+--3 functions
+
+--3.1 general Lua functions
+
+--3.1.1 function checking if file exits
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end --function file_exists(name)
+
+--3.1.2 function escaping magic characters
+function string.escape_forbidden_char(insertstring) --this function takes a string and returns the same string with escaped characters
+	return insertstring:gsub("\\", "\\\\"):gsub("\"", "\\\""):gsub("\'", "\\\'"):gsub("\n", "\\n"):gsub("\r", "\\n")
+end --function string.escape_forbidden_char(insertstring)
+
+--4. dialogs
+--4.1 search dialog
+searchtext = iup.multiline{border="YES",expand="YES", SELECTION="ALL",wordwrap="YES"} --textfield for search
+
+--search in downward direction
+searchdown    = iup.flatbutton{title = "Abwärts",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
+function searchdown:flat_action()
+	local help=false
+	--downward search
+	if checkboxforcasesensitive.value=="ON"  then
+		for i=tree.value + 1, tree.count-1 do
+			if tree["title" .. i]:match(searchtext.value)~= nil then
+				tree.value= i
+				help=true
+				break
+			end --if tree["title" .. i]:match(searchtext.value)~= nil then
+		end --for i=tree.value + 1, tree.count-1 do
+	else
+		for i=tree.value + 1, tree.count-1 do
+			if tree["title" .. i]:lower():match(searchtext.value:lower())~= nil then
+				tree.value= i
+				help=true
+				break
+			end --if tree["title" .. i]:lower():match(searchtext.value:lower())~= nil then
+		end --for i=tree.value + 1, tree.count-1 do
+	end --if checkboxforcasesensitive.value=="ON" then
+	if help==false then
+		iup.Message("Suche","Ende des Baumes erreicht.")
+		tree.value=0 --starting again from the top
+		iup.NextField(maindlg)
+		iup.NextField(dlg_search)
+	end --if help==false then
+end --function searchdown:flat_action()
+
+--search to mark without going to the any node
+searchmark    = iup.flatbutton{title = "Markieren",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
+function searchmark:flat_action()
+	--unmark all nodes
+	for i=0, tree.count - 1 do
+			tree["color" .. i]="0 0 0"
+	end --for i=0, tree.count - 1 do
+	--unmark all nodes end
+	--mark all nodes
+	for i=0, tree.count - 1 do
+		if tree["title" .. i]:upper():match(searchtext.value:upper())~= nil then
+			iup.TreeSetAncestorsAttributes(tree,i,{color="255 0 0",})
+			iup.TreeSetNodeAttributes(tree,i,{color="0 0 250",})
+			iup.TreeSetDescendantsAttributes(tree,i,{color="90 195 0"})
+		end --if tree["title" .. i]:upper():match(searchtext.value:upper())~= nil then
+	end --for i=0, tree.count - 1 do
+	--mark all nodes end
+	for i=0, tree.count - 1 do
+	end --for i=0, tree.count - 1 do
+end --function searchmark:flat_action()
+
+--unmark without leaving the search-window
+unmark    = iup.flatbutton{title = "Entmarkieren",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
+function unmark:flat_action()
+--unmark all nodes
+for i=0, tree.count - 1 do
+	tree["color" .. i]="0 0 0"
+end --for i=0, tree.count - 1 do
+--unmark all nodes end
+end --function unmark:flat_action()
+
+--search in upward direction
+searchup   = iup.flatbutton{title = "Aufwärts",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
+function searchup:flat_action()
+	local help=false
+	--upward search
+	if checkboxforcasesensitive.value=="ON" then
+		for i=tree.value - 1, 0, -1 do
+			if tree["title" .. i]:match(searchtext.value)~= nil then
+				tree.value= i
+				help=true
+				break
+			end --if tree["title" .. i]:match(searchtext.value)~= nil then
+		end --for i=tree.value - 1, 0, -1 do
+	else
+		for i=tree.value - 1, 0, -1 do
+			if tree["title" .. i]:lower():match(searchtext.value:lower())~= nil then
+				tree.value= i
+				help=true
+				break
+			end --if tree["title" .. i]:lower():match(searchtext.value:lower())~= nil then
+		end --for i=tree.value - 1, 0, -1 do
+	end --if checkboxforcasesensitive.value=="ON" then
+	if help==false then
+		iup.Message("Suche","Anfang des Baumes erreicht.")
+		tree.value=tree.count-1 --starting again from the bottom
+		iup.NextField(maindlg)
+		iup.NextField(dlg_search)
+	end --if help==false then
+end --	function searchup:flat_action()
+
+checkboxforcasesensitive = iup.toggle{title="Groß-/Kleinschreibung", value="OFF"} --checkbox for casesensitiv search
+search_label=iup.label{title="Suchfeld:"} --label for textfield
+
+--put above together in a search dialog
+dlg_search =iup.dialog{
+			iup.vbox{iup.hbox{search_label,searchtext,}, 
+
+			iup.label{title="Sonderzeichen: %. für ., %- für -, %+ für +, %% für %, %[ für [, %] für ], %( für (, %) für ), %^ für ^, %$ für $, %? für ?",},
+			iup.hbox{searchmark,unmark,}, 
+			iup.label{title="rot: übergeordnete Knoten",fgcolor = "255 0 0", },
+			iup.label{title="blau: gleicher Knoten",fgcolor = "0 0 255", },
+			iup.label{title="grün: untergeordnete Knoten",fgcolor = "90 195 0", },
+			iup.hbox{searchdown, searchup,checkboxforcasesensitive,},
+
+			}; 
+		title="Suchen",
+		size="420x100",
+		startfocus=searchtext
+		}
+
+--4.1 search dialog end
+
+
+
+--5. context menus (menus for right mouse click)
+
+--5.1 menu of tree
+--5.1.1 copy node of tree
+startcopy = iup.item {title = "Knoten kopieren"}
+function startcopy:action() --copy node
+	 clipboard.text = tree['title']
+end --function startcopy:action()
+
+--5.1.2 start the file or repository of the node of tree
+startnode = iup.item {title = "Starten"}
+function startnode:action() 
+	if tree['title']:match("^.:\\.*%.[^\\ ]+$") or tree['title']:match("^.:\\.*[^\\]+$") or tree['title']:match("^.:\\$") or tree['title']:match("^[^ ]*//[^ ]+$") then 
+		os.execute('start "D" "' .. tree['title'] .. '"') 
+	elseif tree['title']:match("sftp .*") then 
+		os.execute(tree['title']) 
+	end --if tree['title']:match("^.:\\.*%.[^\\ ]+$") or tree['title']:match("^.:\\.*[^\\]+$") or tree['title']:match("^.:\\$") or tree['title']:match("^[^ ]*//[^ ]+$") then 
+end --function startnode:action()
+
+
+
+--5.1.3 put the menu items together in the menu for tree
+menu = iup.menu{
+		startcopy,
+		startnode, 
+		}
+--5.1 menu of tree end
+
+
+--5. context menus (menus for right mouse click) end
+
+
+--6 buttons
+--6.1 logo image definition and button with logo
+img_logo = iup.image{
+  { 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,3,3,1,1,3,3,3,1,1,1,1,1,3,1,1,1,3,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,3,3,1,1,3,1,1,3,1,1,1,1,3,1,1,3,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,3,3,3,3,1,1,1,1,1,3,1,1,3,1,1,1,1,3,1,3,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,3,3,3,4,4,3,1,1,1,1,3,3,3,3,1,1,1,1,3,3,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,3,3,3,3,4,4,3,3,1,1,1,3,1,1,1,3,1,1,1,3,1,3,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,3,3,3,3,3,3,3,3,1,1,1,3,1,1,1,3,1,1,1,3,1,1,3,1,1,1,1,1,4,4,4 },
+  { 4,1,1,3,3,3,3,3,3,3,3,1,1,1,3,3,3,3,1,1,3,1,3,1,1,1,3,1,3,1,1,4,4,4 },
+  { 4,1,1,1,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,3,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,3,1,3,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,3,3,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4,4,1,1,3,3,1,3,1,3,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,1,1,3,3,1,3,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,1,1,1,4,4,4,4,4,3,3,4,4,4,4,1,3,3,1,1,1,1,1,1,1,4,4,4,4 },
+  { 4,1,1,1,1,1,1,1,4,4,4,4,3,3,3,3,3,3,4,4,4,3,1,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,4,4,4,4,4,3,3,3,3,3,3,3,3,3,4,3,4,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,1,1,1,1,1,4,4,4 },
+  { 4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,1,1,1,4,4,4 },
+  { 4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,1,1,4,4,4 },
+  { 4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,4,4,4 },
+  { 4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4 },
+  { 4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4 },
+  { 4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4 },
+  { 4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3 },
+  { 4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4 },
+  { 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4 },
+  { 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 },
+  { 3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 },
+  { 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 }
+  ; colors = { "255 255 255", color_light_color_grey, color_blue, "255 255 255" }
+}
+button_logo=iup.button{image=img_logo,title="", size="23x20"}
+function button_logo:action()
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+end --function button_logo:flat_action()
+
+--6.2 button for screen capture
+button_screenshot=iup.flatbutton{title="Screenshot \nherstellen", size="55x20", BGCOLOR=color_buttons, FGCOLOR=color_button_text}
+function button_screenshot:flat_action()
+	--screen capture
+	--part of screen capture: canvas=cd.CreateCanvas(cd.NATIVEWINDOW,nil)
+	canvas=cd.CreateCanvas(cd.NATIVEWINDOW,nil)
+	canvas:Activate()
+	w,h=canvas:GetSize()
+	image=im.ImageCreate(w*0.8,h,im.RGB,im.BYTE)
+	image:cdCanvasGetImage(canvas,0,0)
+	if tree['TITLE']:lower():match("%.png") then
+		image:Save(tree['TITLE'],"PNG") --.jpg, "JPG" --.png, "PNG" --.bmp,"BMP"
+	else
+		image:Save("screenshot.png","PNG") --.jpg, "JPG" --.png, "PNG" --.bmp,"BMP"
+	end --if tree['TITLE']:lower():match("%.png") then
+	canvas:Kill()
+end --function button_screenshot:flat_action()
+
+--6.3 button for building video from tree images
+button_video=iup.flatbutton{title="Video \nherstellen", size="55x20", BGCOLOR=color_buttons, FGCOLOR=color_button_text}
+function button_video:flat_action()
+	numberOfImages=0
+	new_filename=path .. "\\" .."video.avi"
+	outputfile=im.FileNew(new_filename,"AVI") 
+	outputfile:SetAttribute("FPS",im.FLOAT,{0.1}) --frames per seconds 
+	for i=0,tree.count-1 do
+		if tree['TITLE' .. i]:lower():match("png") then
+			image,err=im.FileImageLoad(tree['TITLE' .. i])
+			err=outputfile:SaveImage(image)
+			numberOfImages=numberOfImages+1
+			print(tree['TITLE' .. i],image,err)
+			image:Destroy()
+			collectgarbage()
+			print(err)
+		end --if tree['TITLE' .. i]:lower():match("png") then
+	end --for i=0,tree.count-1 do
+--]]
+	outputfile:Close()
+	os.execute('"C:\\Program Files\\7-Zip\\7z.exe" a '  .. new_filename:gsub('avi','.zip ') .. " "  .. new_filename .. ' -r ')
+end --function button_video:flat_action()
+
+--6.3.1 button for search in tree
+button_search=iup.flatbutton{title="Suchen\n(Strg+F)", size="45x20", BGCOLOR=color_buttons, FGCOLOR=color_button_text}
+function button_search:flat_action()
+	searchtext.value=tree.title
+	searchtext.SELECTION="ALL"
+	dlg_search:popup(iup.ANYWHERE, iup.ANYWHERE)
+end --function button_search:flat_action()
+
+--6.4 button with second logo
+button_logo2=iup.button{image=img_logo,title="", size="23x20"}
+function button_logo2:action()
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+end --function button_logo:flat_action()
+
+--6 buttons end
+
+
+--7 Main Dialog
+--7.1 load tree from file
+if file_exists(path_documentation_tree) then
+	dofile(path_documentation_tree) --initialize the tree, read from the Lua file
+	for line in io.lines(path_documentation_tree) do
+		if line:match('=')~= nil then 
+			tablename=line:sub(1,line:find('=')-1):gsub(' ', '')
+			break
+		end --if line:match('=')~= nil then 
+	end --for line in io.lines(path_documentation_tree) do
+	--save table in the variable actualtree
+	--Lua 5.1 has the function loadstring() - in later versions, this is replaced by load(), hence we detect this here
+	if _VERSION=='Lua 5.1' then
+		loadstring('actualtree='..tablename)()
+	else
+		load('actualtree='..tablename)() --now actualtree is the table.
+	end --if _VERSION=='Lua 5.1' then
+end --if file_exists(path_documentation_tree) then
+--build tree
+tree=iup.tree{
+map_cb=function(self)
+self:AddNodes(actualtree)
+end, --function(self)
+SIZE="100x100",
+showrename="YES",--F2 key active
+markmode="SINGLE",--for Drag & Drop SINGLE not MULTIPLE
+showdragdrop="YES",
+}
+--set colors of tree
+tree.BGCOLOR=color_background_tree --set the background color of the tree
+-- Callback of the right mouse button click
+function tree:rightclick_cb(id)
+	tree.value = id
+	menu:popup(iup.MOUSEPOS,iup.MOUSEPOS) --popup the defined menue
+end --function tree:rightclick_cb(id)
+-- Callback called when a node will be doubleclicked
+function tree:executeleaf_cb(id)
+	if tree['title' .. id]:match("^.:\\.*%.[^\\ ]+$") or tree['title' .. id]:match("^.:\\.*[^\\]+$") or tree['title' .. id]:match("^.:\\$") or tree['title' .. id]:match("^[^ ]*//[^ ]+$") then os.execute('start "d" "' .. tree['title' .. id] .. '"') end
+end --function tree:executeleaf_cb(id)
+-- Callback for pressed keys
+function tree:k_any(c)
+	if c == iup.K_DEL then
+		-- do a totalchildcount of marked node. Then pop the table entries, which correspond to them.
+		for j=0,tree.totalchildcount do
+			--table.remove(attributes, tree.value+1)
+		end --for j=0,tree.totalchildcount do
+		tree.delnode = "MARKED"
+	elseif c == iup.K_cR then -- expand collapse dialog
+		text_expand_collapse.value=tree.title
+		dlg_expand_collapse:popup(iup.ANYWHERE, iup.ANYWHERE)
+	elseif c == iup.K_cT then -- alphabetic tree sort
+		alphabetic_tree_sort(tree)
+	elseif c == iup.K_cF then
+			searchtext.value=tree.title
+			searchtext.SELECTION="ALL"
+			dlg_search:popup(iup.ANYWHERE, iup.ANYWHERE)
+	elseif c == iup.K_Menu then
+		menu:popup(iup.MOUSEPOS,iup.MOUSEPOS) --popup the defined menue
+	end --if c == iup.K_DEL then
+end --function tree:k_any(c)
+
+
+--7.5 building the dialog and put buttons, trees and preview together
+maindlg = iup.dialog{
+	--simply show a box with buttons
+	iup.vbox{
+		--first row of buttons
+		iup.hbox{
+			button_logo,
+			button_screenshot,
+			button_video,
+			button_search,
+			iup.label{size="10x",},
+			iup.fill{},
+			button_logo2,
+		},
+		
+		iup.hbox{
+			iup.frame{title="Manuelle Zuordnung als Baum",tree,},
+			},
+
+	},
+
+	icon = img_logo,
+	title = path .. " Documentation Tree",
+	SIZE = '220xFULL',
+	BACKGROUND=color_background
+}
+
+--7.5.1 show the dialog
+maindlg:showxy(iup.RIGHT,iup.CENTER)
+
+--7.6 Main Loop
+if (iup.MainLoopLevel()==0) then
+	iup.MainLoop()
+end --if (iup.MainLoopLevel()==0) then
+
