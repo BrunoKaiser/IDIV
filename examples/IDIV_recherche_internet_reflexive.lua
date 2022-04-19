a1272063bc2266f79fdbdec7265b552585e4fe44
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-04-19 09:34:02 +0200

    update IDIV_recherche_internet_reflexive.lua
    update IDIV_recherche_internet_reflexive.lua

--- examples/IDIV_recherche_internet_reflexive.lua
+++ examples/IDIV_recherche_internet_reflexive.lua
@@ -168,8 +168,8 @@ state="COLLAPSED",
  "https://de.wikipedia.org/wiki/Beleihung_(Kreditwesen)",
  "https://de.wikipedia.org/wiki/Kreditwesengesetz",
  "https://de.wikipedia.org/wiki/Bank",
- "https://de.wikipedia.org/wiki/Liste_der_größten_Banken_in_Deutschland",
- "https://de.wikipedia.org/wiki/Monetäre_Finanzinstitute",
+ "https://de.wikipedia.org/wiki/Liste_der_grÃ¶ÃŸten_Banken_in_Deutschland",
+ "https://de.wikipedia.org/wiki/MonetÃ¤re_Finanzinstitute",
  "https://de.wikipedia.org/wiki/Kreditinstitut",
  "https://de.wikipedia.org/wiki/Simulationsbasierte_Optimierung",
  "https://de.wikipedia.org/wiki/Dynamische_Optimierung",
@@ -289,7 +289,7 @@ state="COLLAPSED",
 },
 { branchname="Sprachen",
 state="COLLAPSED",
-{ branchname="Übersetzung",
+{ branchname="Ãœbersetzung",
 state="COLLAPSED",
 { branchname="https://www.babelfish.de",
 state="COLLAPSED",
@@ -308,10 +308,10 @@ state="COLLAPSED",
  "https://www.deepl.com/de/translator",},
 { branchname="https://de.pons.com",
 state="COLLAPSED",
- "https://de.pons.com/übersetzung",},
+ "https://de.pons.com/Ã¼bersetzung",},
 { branchname="https://www.dict.cc",
 state="COLLAPSED",
- "https://www.dict.cc/?s=Übersetzung",},
+ "https://www.dict.cc/?s=Ãœbersetzung",},
 { branchname="https://dict.leo.org",
 state="COLLAPSED",
  "https://dict.leo.org/englisch-deutsch",},
@@ -527,7 +527,7 @@ state="COLLAPSED",
  "https://stackovergo.com/de/tag/iup",},
 { branchname="https://webserver2.tecgraf.puc-rio.br",
 { branchname="https://www.generacodice.com",
- "https://www.generacodice.com/en/articolo/826486/How-to-display-a-progress-Bar-using-Lua-and-IUP", "https://www.generacodice.com/en/articolo/58761/Obtenir-des-contributions-avec-IUP-à-Lua",
+ "https://www.generacodice.com/en/articolo/826486/How-to-display-a-progress-Bar-using-Lua-and-IUP", "https://www.generacodice.com/en/articolo/58761/Obtenir-des-contributions-avec-IUP-Ã -Lua",
 },
 { branchname="https://freebasic.net",
  "https://freebasic.net/forum/viewtopic.php?t=14869",},
@@ -854,7 +854,7 @@ dlg_rename = iup.dialog{
 searchtext = iup.multiline{border="YES",expand="YES", SELECTION="ALL",wordwrap="YES"} --textfield for search
 
 --search in downward direction
-searchdown    = iup.flatbutton{title = "Abwärts",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
+searchdown    = iup.flatbutton{title = "AbwÃ¤rts",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
 function searchdown:flat_action()
 	local help=false
 	--downward search
@@ -934,7 +934,7 @@ end --for i=0, tree.count - 1 do
 end --function unmark:flat_action()
 
 --search in upward direction
-searchup   = iup.flatbutton{title = "Aufwärts",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
+searchup   = iup.flatbutton{title = "AufwÃ¤rts",size="EIGHTH", BGCOLOR=color_buttons, FGCOLOR=color_button_text} 
 function searchup:flat_action()
 	local help=false
 	--upward search
@@ -963,7 +963,7 @@ function searchup:flat_action()
 	end --if help==false then
 end --	function searchup:flat_action()
 
-checkboxforcasesensitive = iup.toggle{title="Groß-/Kleinschreibung", value="OFF"} --checkbox for casesensitiv search
+checkboxforcasesensitive = iup.toggle{title="GroÃŸ-/Kleinschreibung", value="OFF"} --checkbox for casesensitiv search
 checkboxforsearchinfiles = iup.toggle{title="Suche in den Textdateien", value="OFF"} --checkbox for searcg in text files
 search_label=iup.label{title="Suchfeld:"} --label for textfield
 
@@ -987,11 +987,11 @@ end --	function search_in_textbox1:flat_action()
 dlg_search =iup.dialog{
 			iup.vbox{iup.hbox{search_label,searchtext,}, 
 
-			iup.label{title="Sonderzeichen: %. für ., %- für -, %+ für +, %% für %, %[ für [, %] für ], %( für (, %) für ), %^ für ^, %$ für $, %? für ?",},
+			iup.label{title="Sonderzeichen: %. fÃ¼r ., %- fÃ¼r -, %+ fÃ¼r +, %% fÃ¼r %, %[ fÃ¼r [, %] fÃ¼r ], %( fÃ¼r (, %) fÃ¼r ), %^ fÃ¼r ^, %$ fÃ¼r $, %? fÃ¼r ?",},
 			iup.hbox{searchmark,unmark,checkboxforsearchinfiles,}, 
-			iup.label{title="rot: übergeordnete Knoten",fgcolor = "255 0 0", },
+			iup.label{title="rot: Ã¼bergeordnete Knoten",fgcolor = "255 0 0", },
 			iup.label{title="blau: gleicher Knoten",fgcolor = "0 0 255", },
-			iup.label{title="grün: untergeordnete Knoten",fgcolor = "90 195 0", },
+			iup.label{title="grÃ¼n: untergeordnete Knoten",fgcolor = "90 195 0", },
 			iup.hbox{searchdown, searchup,checkboxforcasesensitive,},
 			iup.hbox{search_in_textbox1,},
 
@@ -1023,7 +1023,7 @@ function renamenode:action()
 end --function renamenode:action()
 
 --5.1.3 add branch to tree
-addbranch = iup.item {title = "Ast hinzufügen"}
+addbranch = iup.item {title = "Ast hinzufÃ¼gen"}
 function addbranch:action()
 	tree.addbranch = ""
 	tree.value=tree.value+1
@@ -1031,7 +1031,7 @@ function addbranch:action()
 end --function addbranch:action()
 
 --5.1.3.1 add branch to tree by insertbranch
-addbranchbottom = iup.item {title = "Ast darunter hinzufügen"}
+addbranchbottom = iup.item {title = "Ast darunter hinzufÃ¼gen"}
 function addbranchbottom:action()
 	tree["insertbranch" .. tree.value] = ""
 	for i=tree.value+1,tree.count-1 do
@@ -1044,7 +1044,7 @@ function addbranchbottom:action()
 end --function addbranchbottom:action()
 
 --5.1.3.2 add leaf to tree by insertleaf
-addleafbottom = iup.item {title = "Blatt darunter hinzufügen"}
+addleafbottom = iup.item {title = "Blatt darunter hinzufÃ¼gen"}
 function addleafbottom:action()
 	tree["insertleaf" .. tree.value] = ""
 	for i=tree.value+1,tree.count-1 do
@@ -1088,7 +1088,7 @@ function addleaf_fromclipboardbottom:action()
 end --function addleaf_fromclipboardbottom:action()
 
 --5.1.5 add leaf of tree
-addleaf = iup.item {title = "Blatt hinzufügen"}
+addleaf = iup.item {title = "Blatt hinzufÃ¼gen"}
 function addleaf:action()
 	tree.addleaf = ""
 	tree.value=tree.value+1
@@ -1237,12 +1237,12 @@ menu = iup.menu{
 		startcopy,
 		renamenode, 
 		addbranch, 
-		addbranchbottom, 
 		addbranch_fromclipboard, 
+		addbranchbottom, 
 		addbranch_fromclipboardbottom, 
 		addleaf,
-		addleafbottom,
 		addleaf_fromclipboard,
+		addleafbottom,
 		addleaf_fromclipboardbottom,
 		startversion,
 		startnodescripter, 
