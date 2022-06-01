0ee2a4c484daebb3515747c711bc45fefd473c22
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-06-01 08:14:31 +0200

    add files compare...lua
    add files compare...lua

---
+++ programms/Tree_compare/Git_download_files_to_compare_IDIV.lua
@@ -0,0 +1,287 @@
+
+fileSiteTable={}
+distanceTable={}
+distanceLineNumberTable={}
+distanceLineTable={}
+distanceLineScriptTable={}
+
+fileSiteTable["C:\\Tree\\IDIV_package_starter_kit_zip_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/IDIV_package_starter_kit_zip.lua"
+fileSiteTable["C:\\Tree\\IDIV_package_zip_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/IDIV_package_zip.lua"
+fileSiteTable["C:\\Tree\\CSVtoLua\\CSVtoLua_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/CSVtoLua/CSVtoLua_Linux.lua"
+fileSiteTable["C:\\Tree\\CSVtoLua\\CSVtoLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/CSVtoLua/CSVtoLua_Windows.lua"
+fileSiteTable["C:\\Tree\\ExceltoPDFforLua\\ExceltoPDFforLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/ExceltoPDFforLua/ExceltoPDFforLua_Windows.lua"
+fileSiteTable["C:\\Tree\\ExceltoTXTforLua\\ExceltoTXTforLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/ExceltoTXTforLua/ExceltoTXTforLua_Windows.lua"
+fileSiteTable["C:\\Tree\\Luajava_Tree\\Tree_Luajava_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Luajava_Tree/Tree_Luajava_Linux.lua"
+fileSiteTable["C:\\Tree\\Luajava_Tree\\Tree_Luajava_Linux_RaspberryPi_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Luajava_Tree/Tree_Luajava_Linux_RaspberryPi.lua"
+fileSiteTable["C:\\Tree\\Luajava_Tree\\Tree_Luajava_Linux_directory_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Luajava_Tree/Tree_Luajava_Linux_directory.lua"
+fileSiteTable["C:\\Tree\\Luajava_Tree\\Tree_Luajava_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Luajava_Tree/Tree_Luajava_Windows.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\Tree_LuatoLua_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/Tree_LuatoLua_Linux.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\Tree_LuatoLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/Tree_LuatoLua_Windows.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\Tree_calculator_with_interests_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/Tree_calculator_with_interests.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\Tree_console_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/Tree_console.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\ansicht_documentation_tree_balance_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/ansicht_documentation_tree_balance.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\input_command_output_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/input_command_output_tree.lua"
+fileSiteTable["C:\\Tree\\LuatoLua\\input_command_output_tree_MDI_graphics_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/LuatoLua/input_command_output_tree_MDI_graphics.lua"
+fileSiteTable["C:\\Tree\\PowerpointtoPDFforLua\\PowerpointtoPDFforLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/PowerpointtoPDFforLua/PowerpointtoPDFforLua_Windows.lua"
+fileSiteTable["C:\\Tree\\SQLtoCSVforLua\\SQLtoCSVforLua_SQL_List_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/SQLtoCSVforLua/SQLtoCSVforLua_SQL_List.lua"
+fileSiteTable["C:\\Tree\\TXTtoLua\\bracketstoLua_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/TXTtoLua/bracketstoLua.lua"
+fileSiteTable["C:\\Tree\\TXTtoLua\\brackets_and_semicolontoLua_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/TXTtoLua/brackets_and_semicolontoLua.lua"
+fileSiteTable["C:\\Tree\\TXTtoLua\\TXTtoLua_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/TXTtoLua/TXTtoLua_Linux.lua"
+fileSiteTable["C:\\Tree\\TXTtoLua\\TXTtoLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/TXTtoLua/TXTtoLua_Windows.lua"
+fileSiteTable["C:\\Tree\\Tree_news\\Tree_news_categorisation_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_news/Tree_news_categorisation.lua"
+fileSiteTable["C:\\Tree\\Tree_news\\html_Tree_news_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_news/html_Tree_news_Linux.lua"
+fileSiteTable["C:\\Tree\\Tree_compare\\compare_documentation_tree_lua_trees_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_compare/compare_documentation_tree_lua_trees.lua"
+fileSiteTable["C:\\Tree\\Tree_compare\\compare_documentation_tree_text_files_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_compare/compare_documentation_tree_text_files.lua"
+fileSiteTable["C:\\Tree\\Tree_screenshots\\simple_tree_screenshots_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_screenshots/simple_tree_screenshots.lua"
+fileSiteTable["C:\\Tree\\Tree_screenshots\\tree_video_presentation_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_screenshots/tree_video_presentation.lua"
+fileSiteTable["C:\\Tree\\Word_Tree\\Tree_word_build_luacom_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Word_Tree/Tree_word_build_luacom.lua"
+fileSiteTable["C:\\Tree\\Word_Tree\\Tree_word_build_luacom_local_DE_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Word_Tree/Tree_word_build_luacom_local_DE.lua"
+fileSiteTable["C:\\Tree\\WordtoTXTforLua\\PDFtoTXTforLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/WordtoTXTforLua/PDFtoTXTforLua_Windows.lua"
+fileSiteTable["C:\\Tree\\WordtoTXTforLua\\WordtoTXTforLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/WordtoTXTforLua/WordtoTXTforLua_Windows.lua"
+fileSiteTable["C:\\Tree\\XMLtoLua\\XMLtoLua_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/XMLtoLua/XMLtoLua_Linux.lua"
+fileSiteTable["C:\\Tree\\XMLtoLua\\XMLtoLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/XMLtoLua/XMLtoLua_Windows.lua"
+fileSiteTable["C:\\Tree\\html_Tree\\Tree_html_frame_build_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/html_Tree/Tree_html_frame_build.lua"
+fileSiteTable["C:\\Tree\\html_Tree\\html_build_node_content_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/html_Tree/html_build_node_content.lua"
+fileSiteTable["C:\\Tree\\html_Tree\\html_build_text_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/html_Tree/html_build_text.lua"
+fileSiteTable["C:\\Tree\\htmltoTXTforLua\\htmltoTXTforLua_Windows_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/htmltoTXTforLua/htmltoTXTforLua_Windows.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_of_Lua_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_of_Lua.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_search_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_search.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_with_directory_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_with_directory.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_with_html_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_with_html.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_with_images_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_with_images.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_with_webbrowser_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_with_webbrowser.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_html_with_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_html_with_tree.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_html_with_tree_do_comments_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_html_with_tree_do_comments.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_htmlbook_with_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_htmlbook_with_tree.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_write_html_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_write_html_Linux.lua"
+fileSiteTable["C:\\Tree\\reflexiveDocTree\\reflexive_documentation_tree_write_html_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/reflexiveDocTree/reflexive_documentation_tree_write_html.lua"
+fileSiteTable["C:\\Tree\\simpleDocTree\\simple_documentation_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/simpleDocTree/simple_documentation_tree.lua"
+fileSiteTable["C:\\Tree\\simpleDocTree\\simple_documentation_tree_webpage_search_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/simpleDocTree/simple_documentation_tree_webpage_search.lua"
+fileSiteTable["C:\\Tree\\simpleDocTree\\simple_documentation_tree_with_file_dialog_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/simpleDocTree/simple_documentation_tree_with_file_dialog.lua"
+fileSiteTable["C:\\Tree\\simpleDocTree\\simple_documentation_tree_with_file_dialog_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/simpleDocTree/simple_documentation_tree_with_file_dialog_Linux.lua"
+fileSiteTable["C:\\Tree\\simpleDocTree\\simple_documentation_tree_with_webbrowser_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/simpleDocTree/simple_documentation_tree_with_webbrowser.lua"
+fileSiteTable["C:\\Tree\\simpleDocTree\\simple_documentation_tree_brackets_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/simpleDocTree/simple_documentation_tree_brackets.lua"
+fileSiteTable["C:\\Tree\\wikitextoHtml\\wikiTeXtoHtml_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/wikitextoHtml/wikiTeXtoHtml.lua"
+fileSiteTable["C:\\Tree\\html_Tree\\html_Tree_relative_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/html_Tree_relative.lua"
+fileSiteTable["C:\\Tree\\GUI_Dokumentation_Verzeichnis\\ansicht_documentation_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/ansicht_documentation_tree.lua"
+fileSiteTable["C:\\Tree\\html_Tree\\html_build_Linux_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/html_build_Linux.lua"
+fileSiteTable["C:\\Tree\\html_Tree\\html_build_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/html_Tree/html_build.lua"
+fileSiteTable["C:\\Tree\\GUI_Dokumentation_Verzeichnis\\nachschlagen_documentation_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/nachschlagen_documentation_tree.lua"
+fileSiteTable["C:\\Tree\\htmlTXTtoLua\\html_text_file_to_lua_tree_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/htmlTXTtoLua/html_text_file_to_lua_tree.lua"
+fileSiteTable["C:\\Tree\\htmlTXTtoLua\\html_text_file_to_lua_tree_compared_with_previous_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/htmlTXTtoLua/html_text_file_to_lua_tree_compared_with_previous.lua"
+fileSiteTable["C:\\Tree\\Tree_compare\\Git_download_files_to_compare_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/Tree_compare/Git_download_files_to_compare.lua"
+fileSiteTable["C:\\Tree\\htmlTXTtoLua\\htmlListTreeTXTtoLua_Git.html"]="https://github.com/BrunoKaiser/IDIV/new/main/programms/htmlTXTtoLua/htmlListTreeTXTtoLua.lua"
+
+--3.1.3 general function for distance between texts
+function string.levenshtein(str1, str2)
+	local len1 = string.len(str1)
+	local len2 = string.len(str2)
+	local matrix = {}
+	local cost = 0
+        -- quick cut-offs to save time
+	if (len1 == 0) then
+		return len2
+	elseif (len2 == 0) then
+		return len1
+	elseif (str1 == str2) then
+		return 0
+	end --if (len1 == 0) then
+	-- initialise the base matrix values
+	for i = 0, len1, 1 do
+		matrix[i] = {}
+		matrix[i][0] = i
+	end --for i = 0, len1, 1 do
+	for j = 0, len2, 1 do
+		matrix[0][j] = j
+	end --for j = 0, len2, 1 do
+	-- actual Levenshtein algorithm
+	for i = 1, len1, 1 do
+		for j = 1, len2, 1 do
+			if (str1:byte(i) == str2:byte(j)) then
+				cost = 0
+			else
+				cost = 1
+			end --if (str1:byte(i) == str2:byte(j)) then
+			matrix[i][j] = math.min(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + cost)
+		end --for j = 1, len2, 1 do
+	end --for i = 1, len1, 1 do
+	-- return the last value - this is the Levenshtein distance
+	return matrix[len1][len2]
+end --function string.levenshtein(str1, str2)
+
+
+for k,v in pairs(fileSiteTable) do
+	local outputfile1=io.open(k:gsub("%.html$",".txt"),"w")
+	os.execute('curl -o ' .. k .. ' ' .. v .. ' ')
+	local inputfile1=io.open(k,"r")
+	if inputfile1 then
+		inputText=inputfile1:read("*all")
+		inputfile1:close()
+		for line in inputText:gmatch("line%-number=.-data%-") do 
+			local lineNumber,lineText=(line:gsub("</td>\n          <td","</td><td")
+							:gsub('line%-number="(%d+">)',"%1")
+							:gsub('<.->',"")
+							:gsub('<td.*$',"")
+							:gsub('<svg.*$',"")
+							:gsub('&amp;','&') --before &quot; to change it to "
+							:gsub('&amp;','&') --to be able to change &amp; into &
+							:gsub('&quot;','"')
+							:gsub('&lt;','<')
+							:gsub('&gt;','>')
+							:gsub('&#39;',"'")
+							:gsub("\n","")
+							:gsub("�€ž","'")
+							:gsub("�€œ","'")
+							:gsub("�€š","'")
+							:gsub("�€˜","'")
+							:gsub("Ã¤","ä")
+							:gsub("ä","�")
+							:gsub("Ã„","Ä")
+							:gsub("Ä","�")
+							:gsub("Ã¶","ö")
+							:gsub("ö","�")
+							:gsub("Ã–","Ö")
+							:gsub("Ö","�")
+							:gsub("Ã¼","ü")
+							:gsub("ü","�")
+							:gsub("Ãœ","Ü")
+							:gsub("Ü","�")
+							:gsub("ÃŸ","ß")
+							:gsub("ß","�")
+							:gsub(" "," ")
+							:gsub("×","�")
+							:gsub("÷","�")
+							:gsub("é","�")
+							:gsub("î","�")
+							:gsub("ê","�")
+							:gsub("â","�")
+							:gsub("ç","�")
+							:gsub("ô","�")
+							:gsub("à","�")
+							:gsub("â€“","–")	
+							:gsub("â€ž", "„")
+							:gsub("„", "�")
+							:gsub("�", "�")
+							:gsub("�", " ")
+							:gsub("ÿ", " ")
+							:gsub("â€œ", "“")
+							:gsub("â€š", "‚")
+							:gsub("â€˜", "‘")
+							:gsub("Â ",     " ")
+							:gsub("Ã©",    "é")
+							:gsub("è","�")
+							:gsub("ùd��ø89:;2§","�d���89:;2�")
+							:gsub("–","�")
+							:gsub("�€“","-")
+							:gsub(" "," ")
+							:gsub("é","�")
+							:gsub("�€ž","�")
+							:gsub("�€œ","'")
+							:gsub("�€š","'")
+							:gsub("�€˜","'")
+							:gsub("Ã—","�")
+							:gsub("Ã·","�")
+							:gsub("Ã§","�")
+							:gsub("Ã´","�")
+							:gsub("Ã¨","�")
+							:gsub("Ã ","�")
+							)
+								:match('(%d+)">(.*)')
+			--test with: print(lineNumber,lineText)
+			outputfile1:write(lineText:gsub(" +$","") .. "\n")
+		end --for line in inputText:gmatch("line%-number=.-data%-") do 
+		outputfile1:close()
+		--compare with Lua script line by line
+		local LuaScriptLineTable={}
+		local fileNameLua=k:gsub("_Git%.html$",".lua")
+		for line in io.lines(fileNameLua) do
+			LuaScriptLineTable[#LuaScriptLineTable+1]=line:gsub(" +$","")
+									:gsub('&amp;','&')
+									:gsub('&quot;','"')
+									:gsub('&lt;','<')
+									:gsub('&gt;','>')
+									:gsub('&#39;',"'")
+									:gsub("Ã¤","ä")
+									:gsub("ä","�")
+									:gsub("Ä","�")
+									:gsub("Ã„","Ä")
+									:gsub("ö","�")
+									:gsub("Ã¶","ö")
+									:gsub("Ö","�")
+									:gsub("Ã–","Ö")
+									:gsub("ü","�")
+									:gsub("Ã¼","ü")
+									:gsub("Ü","�")
+									:gsub("Ãœ","Ü")
+									:gsub("ß","�")
+									:gsub("ÃŸ","ß")
+									:gsub(" "," ")
+									:gsub("×","�")
+									:gsub("÷","�")
+									:gsub("é","�")
+									:gsub("ç","�")
+									:gsub("ô","�")
+									:gsub("à","�")
+									:gsub("–","-")
+									:gsub("â€“","–")	
+									:gsub("â€ž", "„")
+									:gsub("„", "�")
+									:gsub("�", "�")
+									:gsub("�", " ")
+									:gsub("ÿ", " ")
+									:gsub("â€œ", "“")
+									:gsub("â€š", "‚")
+									:gsub("â€˜", "‘")
+									:gsub("Â ",     " ")
+									:gsub("Ã©",    "é")
+									:gsub("è","�")
+									:gsub("“","'")
+									:gsub("‚","'")
+									:gsub("‘","'") 
+									:gsub("�"," ") --blank without space character
+
+		end --for line in io.lines(fileNameLua) do
+		local lineNumber=0
+		local distance=0
+		local lineNumberOfDistance=0
+		local lineOfDistance=""
+		local LuaScriptLineOfDistance=""
+		local fileNameTXT=k:gsub("_Git%.html$","_Git.txt")
+		for line in io.lines(fileNameTXT) do
+			lineNumber=lineNumber+1
+			--test with: if lineNumber==1123 then print(lineNumber,line, LuaScriptLineTable[lineNumber]) end
+			if LuaScriptLineTable[lineNumber]==nil then
+				LuaScriptLineTable[lineNumber]=""
+			end -- if LuaScriptLineTable[lineNumber]==nil then
+			if string.levenshtein(line, LuaScriptLineTable[lineNumber])>distance then
+				distance=string.levenshtein(line, LuaScriptLineTable[lineNumber])
+				lineNumberOfDistance=lineNumber
+				lineOfDistance=line
+				LuaScriptLineOfDistance=LuaScriptLineTable[lineNumber]
+			end --if string.levenshtein(line, LuaScriptLineTable[lineNumber])>distance then
+		end --for line in io.lines(fileNameTXT) do
+		distanceTable[k]=distance
+		distanceLineNumberTable[k]=lineNumberOfDistance
+		distanceLineTable[k]=lineOfDistance
+		distanceLineScriptTable[k]=LuaScriptLineOfDistance
+	end --if inputfile1 then
+end --for k,v in pairs(fileSiteTable) do
+
+distanceSortedTable={}
+for k,v in pairs(distanceTable) do
+	distanceSortedTable[#distanceSortedTable+1]=  string.rep(" ",math.max(8-#tostring(v),0)) .. v 
+							.. ": \tErste maximale Distanz-Zeile" ..  string.rep(" ",math.max(8-#tostring(distanceLineNumberTable[k]),0)) 
+							.. distanceLineNumberTable[k] .. ": \t" 
+							.. k  .. string.rep(" ",math.max(math.floor(20-#k%10,0))) .. string.rep("\t",math.max(math.floor(10-#k/9),0))
+							.. ": " .. fileSiteTable[k]
+							.. "   :    " .. distanceLineScriptTable[k]
+							.. "   :    " .. distanceLineTable[k]
+end --for k,v in pairs(distanceTable) do
+table.sort(distanceSortedTable,function(a,b) aT=tonumber(a:match("(%d+):")) bT=tonumber(b:match("(%d+):")) return aT<bT end)
+
+for i,v in pairs(distanceSortedTable) do
+	print(i,v)
+end --for i,v in pairs(distanceSortedTable) do
+
+	
