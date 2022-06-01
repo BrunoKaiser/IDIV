07e7dd7c8412ee023f0c561ea963e6d9b055eade
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-06-01 08:39:03 +0200

    add repository with_frames_3
    add repository with_frames_3

---
+++ programms/html_Tree/with_frames_3/Tree_html_frame_start.bat
@@ -0,0 +1,7 @@
+
+echo ^<pre^> > C:\Tree\html_Tree\with_frames_3\Tree_html_frame_home.html
+dir * /b/o/s >> C:\Tree\html_Tree\with_frames_3\Tree_html_frame_home.html
+echo ^</pre^> >> C:\Tree\html_Tree\with_frames_3\Tree_html_frame_home.html
+
+
+start "D" "C:\Tree\html_Tree\with_frames_3\Tree_html_frame.html"
