43fcb213396a58865c7caec0ce8626311ffcc3d8
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-06-01 08:38:17 +0200

    add repository with_frames_2
    add repository with_frames_2

---
+++ programms/html_Tree/with_frames_2/Tree_html_frame_start.bat
@@ -0,0 +1,8 @@
+
+echo ^<pre^> > C:\Tree\html_Tree\with_frames_2\Tree_html_frame_home.html
+echo This frame html contains a tree in the left frame. The mark functionality is limited because of the denomination of the link names. Go to link is not possible if mark functions correctly and vice versa. >> C:\Tree\html_Tree\with_frames_2\Tree_html_frame_home.html
+dir * /b/o/s >> C:\Tree\html_Tree\with_frames_2\Tree_html_frame_home.html
+echo ^</pre^> >> C:\Tree\html_Tree\with_frames_2\Tree_html_frame_home.html
+
+
+start "D" "C:\Tree\html_Tree\with_frames_2\Tree_html_frame.html"
