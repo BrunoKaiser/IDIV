c88657327251f62fcb4c32c627ec964083120cfa
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-01-12 18:41:26 +0100

    update simple_doc...filed.lua
    update simple_doc...filed.lua

--- programms/simpleDocTree/simple_documentation_tree_with_variables_to_be_filed.lua
+++ programms/simpleDocTree/simple_documentation_tree_with_variables_to_be_filed.lua
@@ -28,14 +28,14 @@ for line in io.lines("C:\\Tree\\simpleDocTree\\simple_documentation_tree_with_va
 		lineRead="no"
 	elseif lineRead=="yes" and line:match("=") then
 		VariablendefiniertTabelle[line:match("(.*)="):gsub(" ","")]="defined"
-	elseif lineRead=="yes" and line:match('"')==nil then
+	elseif lineRead=="yes" and line:match('"')==nil and line:match('^%-%-')==nil then
 		if VariablendefiniertTabelle[line:gsub("\t",""):gsub("}",""):gsub(",",""):gsub(" ","")]==nil 
 			and line:gsub("\t",""):gsub("}",""):gsub(",",""):gsub(" ","")~="" then
 			print(line:gsub("\t",""):gsub("}",""):gsub(",",""):gsub(" ","") .. "\n    fehlt")
 		end --if VariablendefiniertTabelle[k]==nil then
 		VariablenNoetigTabelle[line:gsub("\t",""):gsub("}",""):gsub(",",""):gsub(" ","")]="needed"
 	end --if line:match("^%-%-Programm") then
-end --for line in io.lines("P:\DIS-WS\CrisDa - Phase 3\07 SAS\Programm_Analyse\SAS_Seiteneffektketten.lua") do
+end --for line in io.lines("C:\\Tree\\simpleDocTree\\simple_documentation_tree_with_variables_to_be_filed.lua") do
 
 --[[test with:
 for k,v in pairs(VariablenNoetigTabelle) do
