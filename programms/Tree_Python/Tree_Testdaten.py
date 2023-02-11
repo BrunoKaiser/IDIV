629ef005e2ae7d64af9f1b0ce526c54e2e7f4bb9
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-02-11 08:29:41 +0100

    add file Tree_Testdaten.py
    add file Tree_Testdaten.py

---
+++ programms/Tree_Python/Tree_Testdaten.py
@@ -0,0 +1,34 @@
+# This script scontains a tree defined as xml shown in a graphical user interface and a file browser tree
+
+
+# 2. data of the tree
+data= r'''
+<IDIV>
+<Dateien>
+<file_Python_Tree_Testdaten>C:\Tree\Tree_Python\Tree_Testdaten.py</file_Python_Tree_Testdaten>
+<file_Python_lupaExecuteLuainPython>C:\Tree\Tree_Python\lupaExecuteLuainPython.py</file_Python_lupaExecuteLuainPython>
+<file_Python_tree_xml_python3_windows>C:\Tree\Tree_Python\tree_xml_python3_windows.py</file_Python_tree_xml_python3_windows>
+<file_Lua>C:\Tree\Tree_Python\test.lua</file_Lua>
+<file_Text>C:\Tree\Tree_Python\test.txt</file_Text>
+<file_pdf>C:\Temp\Bauknecht_Datenschutz.pdf</file_pdf>
+
+</Dateien>
+
+<Verzeichnisse>
+<Ordner>/Tree/Tree_Python</Ordner>
+<Ordner>/Tree</Ordner>
+
+
+</Verzeichnisse>
+
+
+<Tree-Programme>
+
+<file>C:\Tree\Tree_Python\tree_xml_python.py</file>
+
+</Tree-Programme>
+
+</IDIV>
+
+'''
+
