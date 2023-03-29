723a2fddf0aff917d6c411f680441079d82625d0
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:37:50 +0200

    update reflexive_doc...tree_with_dataform.lua
    update reflexive_documentation_tree_with_dataform.lua

--- programms/reflexiveDocTree/reflexive_documentation_tree_with_dataform.lua
+++ programms/reflexiveDocTree/reflexive_documentation_tree_with_dataform.lua
@@ -960,7 +960,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for saving DataTable and the programm of the graphical user interface
@@ -1010,7 +1010,7 @@ end --function button_open_tree_from_database:flat_action()
 --6.6 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --7 Main Dialog
