fd7d71e78cc48a8dc0f5c390463a9ec3e5aca591
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:46:55 +0200

    update Tree_news_categorisation.lua
    update Tree_news_categorisation.lua

--- programms/Tree_news/Tree_news_categorisation.lua
+++ programms/Tree_news/Tree_news_categorisation.lua
@@ -1018,7 +1018,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for saving tree
@@ -1214,7 +1214,7 @@ end --function button_goto_page:flat_action()
 --6.7 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6 buttons end
