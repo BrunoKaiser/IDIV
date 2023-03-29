5610d5a085fbc1f82f7a8228b9e4e5b42db18483
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:23:33 +0200

    update simple_datapart_tree_with_ticks.lua
    update simple_datapart_tree_with_ticks.lua

--- programms/simpleDocTree/simple_datapart_tree_with_ticks.lua
+++ programms/simpleDocTree/simple_datapart_tree_with_ticks.lua
@@ -492,7 +492,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2.1 button for saving tree
@@ -697,7 +697,7 @@ end --function button_delete_tree:flat_action()
 --6.9 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6 buttons end
