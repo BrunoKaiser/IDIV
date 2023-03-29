f48a3585820f383f9ca6d16fe0aa9d321a33cf4c
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:29:35 +0200

    update simple_documentation_tree_sqlite.lua
    update simple_documentation_tree_sqlite.lua

--- programms/simpleDocTree/simple_documentation_tree_sqlite.lua
+++ programms/simpleDocTree/simple_documentation_tree_sqlite.lua
@@ -564,7 +564,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for saving tree
@@ -679,7 +679,7 @@ end --function button_saving_tree_in_database:flat_action()
 --6.8 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6 buttons end
