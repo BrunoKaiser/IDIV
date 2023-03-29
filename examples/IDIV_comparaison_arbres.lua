bccaddcfb2f4aeeb068a79e42069072a4f4e8e95
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:05:00 +0200

    update IDIV_comparaison_arbres.lua
    update IDIV_comparaison_arbres.lua

--- examples/IDIV_comparaison_arbres.lua
+++ examples/IDIV_comparaison_arbres.lua
@@ -379,7 +379,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for loading tree 1
@@ -919,7 +919,7 @@ end --function button_save_lua_table:flat_action()
 --6.12 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6 buttons end
