11a2ff6853475d0305e91d33bb9dded95cfb33dc
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:42:26 +0200

    update reflexive_documentation_tree_write_html.lua
    update reflexive_documentation_tree_write_html.lua

--- programms/reflexiveDocTree/reflexive_documentation_tree_write_html.lua
+++ programms/reflexiveDocTree/reflexive_documentation_tree_write_html.lua
@@ -660,7 +660,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for loading tree
@@ -744,7 +744,7 @@ end --function button_expand_collapse_dialog:flat_action()
 --6.6 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6. buttons end
