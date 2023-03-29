66360a73b540ed31d05574756d3183aef706bcf4
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:19:54 +0200

    update Tree_Baum.lua
    update Tree_Baum.lua

--- examples/Tree_Baum.lua
+++ examples/Tree_Baum.lua
@@ -1,54 +1,11 @@
-Tree={branchname="ESG-Artikel-Links",
-
-"https://www.pwc.de/de/managementberatung/forensic-services/esg-fraud/esg-risk-assessments.html",
-
-"https://www.boersen-zeitung.de/banken-finanzen/auf-der-suche-nach-den-esg-daten-eb3921aa-ba3e-11ec-8e62-5f7cf1d225cc",
-
-
-"https://www.msn.com/de-de/finanzen/top-stories/esg-risikomanagement-neue-gefahren-f-c3-bcrs-gesch-c3-a4ft/ar-AAWjiX7",
-
-"https://klardenker.kpmg.de/digital-hub/esg-risikomanagement-neue-ansaetze-fuer-banken/",
-
-
+Tree={branchname="Artikel-Links",
 {branchname="https://www.ppi.de/banken/banksteuerung-und-risikomanagement/esg-risikomanagement/",
-
-"/mnt/sdcard/Downloads/PPI-Onepager-ESG-Quick-K2.pdf",
-"/mnt/sdcard/Downloads/PPI-Onepager-ESG-ReadinessCheck.pdf",
-"/mnt/sdcard/Downloads/csm_20210416_PPI_WP-ESG_Risiken_Grafik_Transparent_52f697f3bb.webp" ,
-
-"/mnt/sdcard/Downloads/20220322_PPI_DP_Taxonomie.pdf",
-
-"Green Asset Ratio",
-
-
-"Im Ergebnis gleicht die EU-Taxonomie im Hinblick auf Erdgas und Kernkraft der regulatorische  Zulässigkeit von AAA-ABS-Strukturen vor der jüngsten großen Finanzkrise der Jahre 2007/2008. S. 7",
-
 "https://www.geldinstitute.de/business/2020/fuer-stuermische-zeiten-wappnen.html",
 "https://www.geldinstitute.de/business/2020/den-damm-rechtzeitig-verstaerken.html",
-
-
 "Szenarioanalysen, Stressfallsimulationen und Auswirkungsszenarien",
-
-
-"ESG-Masterkatalog in Form eines Templates",
-
 "Subscores",
-
 "https://www.geldinstitute.de/business/2021/praktische-ueberlegungen---scoring-modell-und-stresstests.html",
-
-
-
-
 },
-
-{branchname="https://www.mazars.de/Home/ber-uns/Aktuelles/Presse-Medien/Publikationen/Studien/Archiv-2019/ESG-Risiken-im-Risikomanagement",
-"COSO",
-"WBCSD",
-
-},
-
-"/mnt/sdcard/Executive Summary - Studie Riskmanagement.pdf",
-
 }
 
 
