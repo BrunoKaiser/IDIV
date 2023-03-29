6ed3195218925dc696af8c8d5be80bcefe21d52f
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:24:20 +0200

    update scintilla_datapart.lua
    update scintilla_datapart.lua

--- programms/simpleDocTree/scintilla_datapart.lua
+++ programms/simpleDocTree/scintilla_datapart.lua
@@ -219,7 +219,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for saving tree
@@ -245,7 +245,7 @@ end --function button_replace:flat_action()
 --6.5 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6 buttons end
