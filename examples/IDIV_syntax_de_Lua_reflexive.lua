795e132eba41fc44c996826c4460741249dcb6d6
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2023-03-29 22:16:18 +0200

    update IDIV_syntax_de_Lua_reflexive.lua
    update IDIV_syntax_de_Lua_reflexive.lua

--- examples/IDIV_syntax_de_Lua_reflexive.lua
+++ examples/IDIV_syntax_de_Lua_reflexive.lua
@@ -6142,7 +6142,7 @@ img_logo = iup.image{
 }
 button_logo=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --6.2 button for saving TextHTMLtable and the programm of the graphical user interface
@@ -6287,7 +6287,7 @@ end --function button_new_page:action()
 --6.12 button with second logo
 button_logo2=iup.button{image=img_logo,title="", size="23x20"}
 function button_logo2:action()
-	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nb.kaiser@beckmann-partner.de")
+	iup.Message("Dr. Bruno Kaiser","Lizenz Open Source\nidiv.kaiser@t-online.de")
 end --function button_logo:flat_action()
 
 --7 Main Dialog
